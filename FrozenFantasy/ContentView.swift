0a715fc2cfbec28a6927555d2adf0e8f7f2c66d3
Nikita Sigal <nikitasigal@icloud.com>
2024-03-03 16:32:04 +0300

    Initial Commit

---
+++ FrozenFantasy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FrozenFantasy
+//
+//  Created by Никита Сигал on 03.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FrozenFantasy/FrozenFantasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrozenFantasyApp.swift
+//  FrozenFantasy
+//
+//  Created by Никита Сигал on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FrozenFantasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
