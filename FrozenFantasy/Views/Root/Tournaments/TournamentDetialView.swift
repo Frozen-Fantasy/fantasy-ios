09ad1e84be081e415d5e961818b59f6b5b809192
Nikita Sigal <nikitasigal@icloud.com>
2024-04-22 07:23:41 +0300

    Implelemted the Tournaments Screen, API and Models

---
+++ FrozenFantasy/Views/Root/Tournaments/TournamentDetialView.swift
@@ -0,0 +1,24 @@
+//
+//  TournamentDetialView.swift
+//  FrozenFantasy
+//
+//  Created by Никита Сигал on 22.04.2024.
+//
+
+import SwiftUI
+
+struct TournamentDetialView: View {
+    init(_ tournament: Tournament) {
+        
+    }
+    
+    var body: some View {
+        Text("Tournament Detail Placeholder")
+            .font(.customTitle1)
+            .foregroundStyle(.customBlack)
+    }
+}
+
+#Preview {
+    TournamentDetialView(.init())
+}
